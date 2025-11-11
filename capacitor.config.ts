9bbf8f80e288a315243505fce43f78aef459dc8c
kernel13 <troop_deputy.0a@icloud.com>
2025-11-11 16:11:18 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'oneMinuteSkills',
+  webDir: 'www'
+};
+
+export default config;
